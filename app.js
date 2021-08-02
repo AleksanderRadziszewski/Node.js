d9cc87681d7bcdef2853db88a84d177e15a4b1ca
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-08-02 15:45:38 +0200

    exports, modules

---
+++ app.js
@@ -0,0 +1,9 @@
+// Modules
+const names = require ('./2-names.js');
+const {peter} = require ('./2-names.js');
+const sayHi = require('./3-utils');
+
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(peter)
