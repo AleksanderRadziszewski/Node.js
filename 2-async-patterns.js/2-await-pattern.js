6d309143dee12bb71ed6f71de447428a6852f9ae
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2021-08-14 17:12:39 +0200

    util module, fs.promises, 2x read and 1xwrite

--- 2-async-patterns.js/2-await-pattern.js
+++ 2-async-patterns.js/2-await-pattern.js
@@ -1,35 +1,37 @@
-const { readFile } = require('fs');
-
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf-8', (err, data) => {
-            if (err) {
-                reject(err)
-                return
-            }
-            else {
-                resolve(data)
-            }
-        })
-    })
-
-}
+const { readFile, writeFile } = require('fs').promises
 
 const start = async () => {
     try {
-        const first = await getText('./content/first.txt');
-        console.log(first)
+        const first = await readFile('./content/first.txt', 'utf-8')
+        const second = await readFile('./content/second.txt', 'utf-8')
+        await writeFile(
+            './content/result-mind-grenade.txt',
+            `THIS IS AWESOME: ${first}${second}`, {flag:'a'})
+        console.log(first, second)
+       
 
     } catch (error) {
         console.log(error)
 
     }
-
-
 }
-
 start()
 
-// getText('./content/first.txt')
-//     .then((result) => console.log(result))
-//     .catch((err) => console.log(err))
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+// const getText = (path) => {
+    //     return new Promise((resolve, reject) => {
+    //         readFile(path, 'utf-8', (err, data) => {
+    //             if (err) {
+    //                 reject(err)
+    //                 return
+    //             }
+    //             else {
+    //                 resolve(data)
+    //             }
+    //         })
+    //     })
+    
+    // }
